00ec44b4632748bafdc6a0d4c481eb0514fd17e2
pranavkapoor0808 <pranavparaskapoor@gmail.com>
2021-03-12 01:42:17 -0500

    Add run script and swagger fix

--- src/main/java/edu/umass/fetch/swagger/SpringFoxConfig.java
+++ src/main/java/edu/umass/fetch/swagger/SpringFoxConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -27,6 +28,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+
     registry
         .addResourceHandler("swagger-ui.html")
         .addResourceLocations("classpath:/META-INF/resources/");
@@ -34,6 +36,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     registry
         .addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "swagger-ui.html");
   }
 
 }
